efed8b215dc956cf985ee5420dcdb9b4136465a8
Eric Ishii Eckhardt <eric@adaptedstudio.com>
2012-07-20 15:02:30 -0400

    hacked remote
    Signed-off-by: Eric Ishii Eckhardt <eric@adaptedstudio.com>

--- VideoPlayer/Web/labvideo/js/labvideo.js
+++ VideoPlayer/Web/labvideo/js/labvideo.js
@@ -60,11 +60,18 @@ LAB.getScriptPath = function(filename) {
 	return "";
 };
 
+labvideo.src = "js/";
 LAB.require(labvideo.src+"Events.js");
 LAB.require(labvideo.src+"BaseVideoModel.js");
 LAB.require(labvideo.src+"BaseVideoView.js");
 LAB.require(labvideo.src+"BaseVideoController.js");
 LAB.require(labvideo.src+"BasePlaylistController.js");
-
+/*
+LAB.require(labvideo.src+"Events.js");
+LAB.require(labvideo.src+"BaseVideoModel.js");
+LAB.require(labvideo.src+"BaseVideoView.js");
+LAB.require(labvideo.src+"BaseVideoController.js");
+LAB.require(labvideo.src+"BasePlaylistController.js");
+*/
 
 
