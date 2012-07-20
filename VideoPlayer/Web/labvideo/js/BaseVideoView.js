ff5e3aac5ee7824677b558a06379568bfa012540
robotconscience <brett@robotconscience.com>
2012-07-20 15:53:37 -0400

    Added comments to view

--- VideoPlayer/Web/labvideo/js/BaseVideoView.js
+++ VideoPlayer/Web/labvideo/js/BaseVideoView.js
@@ -2,30 +2,65 @@
 var LoopType = {
 	LOOP_REPEAT:"loop_repeat",
 	LOOP_BACK_AND_FORTH:"loop_back_and_forth"
-};
+}
 
 // All functions to do with display
 var BaseVideoView = function(){
 	// what properties should this have?
 }
 
+/** 
+* @function
+* @param {DOMElement} parentElement
+*/
+BaseVideoView.prototype.setup = function( parent ){};
+
 // called by Controller
+/** 
+* @function
+* @param {String} url
+* @returns {Boolean} Success (or not)
+*/
+BaseVideoView.prototype.load = function( url ){};
+
+/** 
+* @function
+* @returns {Boolean} Success?
+*/
 BaseVideoView.prototype.start = function(){};
+
+/** 
+* Stop + Seek to 0
+* @function
+*/
 BaseVideoView.prototype.stop = function(){};
+
+/** 
+* @function
+*/
 BaseVideoView.prototype.pause = function(){};
 
-BaseVideoView.prototype.load = function( url ){
-	return true;
-}
+/** 
+* @function
+*/
+BaseVideoView.prototype.unload = function(){}
 
-BaseVideoView.prototype.unload = function(){
-	return true;
-}
-
+/** 
+* @function
+* @param	{Float} where (0-1)
+*/
 BaseVideoView.prototype.seek = function( where ){}; //0-1
 
 // GUI -> Controller -> View
+/** 
+* @function
+*/
 BaseVideoView.prototype.goFullscreen 	= function(){};
+
+/** 
+* @function
+* @param	{Float} level (0-1)
+*/
 BaseVideoView.prototype.setVolume 		= function( level ){}; //0-1
 
 // Metadata
@@ -33,9 +68,7 @@ BaseVideoView.prototype.setVolume 		= function( level ){}; //0-1
 * @function
 * @param 	{BaseMetaData }	metadata
 */
-
-BaseVideoView.prototype.setMetadata = function( metadata){
-}
+BaseVideoView.prototype.setMetadata = function( metadata){}
 
 /** 
 * @function
@@ -56,11 +89,40 @@ BaseVideoView.prototype.setDescription = function( description ){};
 BaseVideoView.prototype.addThumbnail = function( thumb ){};
 
 // Effects
+/** 
+* @function
+* @returns 	{Array}	Array of BaseVideoEffect(s)
+*/
 BaseVideoView.prototype.getEffects 	= function(){};
+
+/** 
+* @function
+* @param 	{Float}	speed -5 to 5 ? (range tbd)
+*/
 BaseVideoView.prototype.setSpeed 	= function( speed ){};
+
+/** 
+* @function
+* @param 	{BaseVideoEffect}	effect
+*/
 BaseVideoView.prototype.addEffect 	= function( effect ){};
+
+/** 
+* @function
+* @param 	{BaseVideoEffect}	effect
+*/
 BaseVideoView.prototype.removeEffect = function( effect ){};
 
 // Getters
+
+/** 
+* @function
+* @returns 	{BaseVideoStae}
+*/
 BaseVideoView.prototype.getState = function(){};
+
+/** 
+* @function
+* @returns 	{Integer}
+*/
 BaseVideoView.prototype.getTime = function(){};
